5a0eac72fc681408801575e172f341ac845c8547
Zoren X <47592497+hansfzlorenzana@users.noreply.github.com>
2023-05-06 09:11:02 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from flask import Flask
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+from datetime import datetime
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+messages = ['Hi','How are you?','I\'m fine']
+
+
+df = pd.read_csv('message.csv')
+
+df_old = df
+
+new_data = []
+
+for i in messages:
+    now = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+    message = i
+
+    new_data.append([now,
+                     message])
+    
+
+df_new = pd.DataFrame(new_data,columns=['date_time','message'])
+
+df_gathered = pd.concat([df_old,df_new])
+
+df_gathered.to_csv('message.csv', index=False)
+
+# Generate a chart
+
+fig, ax = plt.subplots(1)
+
+x = df_gathered['message'].value_counts()['Hi']
+y = df_gathered['message'].value_counts()['How are you?']
+
+ax.plot(x,random.randint(0,10),'bo')
+
+plt.savefig('test.png')
